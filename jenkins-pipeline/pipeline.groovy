3ccb0a4f3f7812cb08e4214b7f32df8ddc6e5f59
Mads Nielsen <man@praqma.net>
2018-08-02 09:19:50 +0200

    Added Jenkinsfile

---
+++ jenkins-pipeline/pipeline.groovy
@@ -0,0 +1,32 @@
+multibranchPipelineJob("Codesonar Jenkins Plugin") {
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('jenkins-pipeline/Jenkinsfile')
+        }
+    }
+    branchSources {
+        git {
+            credentialsId("github")
+            remote("https://github.com/Praqma/drmemory-plugin.git")
+        }
+
+        triggers {
+            periodic(20)
+        }
+    }
+
+    configure {
+        def traitBlock = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits' 
+        traitBlock << 'jenkins.plugins.git.traits.CloneOptionTrait' {
+            extension(class: 'hudson.plugins.git.extensions.impl.CloneOption') {
+                shallow(false)
+                noTag(false)
+                reference()
+                depth(0)
+                honorRefspec(false)
+            }
+        }
+
+        traitBlock << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' { }
+    }
+}
